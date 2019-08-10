bd7513213bed5ada4a4e55178e9711f8c0e12a60
scott <banchisme@gmail.com>
2019-08-10 18:22:22 -0500

    added procastination

--- clickfeatures/procrastination.py
+++ clickfeatures/procrastination.py
@@ -6,22 +6,22 @@ import timeseries
 import collections
 
 class WeightedMean(object):
-	def __init__(self, weight_function, *args, **kwargs):
-		r"""
-		Argument:
-			weight_function (fun), a (start, end) -> (0, inf) function
-		"""
-		self.f = weight_function
-		self.ts = timeseries.TimeSeries(*args, **kwargs)
+    def __init__(self, weight_function, *args, **kwargs):
+        r"""
+        Argument:
+            weight_function (fun), a (start, end) -> (0, inf) function
+        """
+        self.f = weight_function
+        self.ts = timeseries.TimeSeries(*args, **kwargs)
 
-	def get_procrastination(self, normalize_time=True, normalize_weight=True):
-		timestamps = np.array(self.ts.get_timestamps(), dtype=np.float)
-		if normalize_time:
-			timestamps = timestamps / self.ts.get_duration()
+    def get_procrastination(self, normalize_time=True, normalize_weight=True):
+        timestamps = np.array(self.ts.get_timestamps(), dtype=np.float)
+        if normalize_time:
+            timestamps = timestamps / self.ts.get_duration()
 
-		effort = np.array(self.ts.get_weights(), dtype=np.float)
-		if normalize_weight:
-			effort = effort / effort.sum()  # normalize
+        effort = np.array(self.ts.get_weights(), dtype=np.float)
+        if normalize_weight:
+            effort = effort / effort.sum()  # normalize
 
-		weight = np.array(map(self.f, timestamps), dtype=np.float)
-		return np.dot(weight, effort)
+        weight = np.array(map(self.f, timestamps), dtype=np.float)
+        return np.dot(weight, effort)
--- tests/test_procrastination.py
+++ tests/test_procrastination.py
@@ -8,10 +8,10 @@ from scipy import stats, fftpack
 
 
 def test_weighted_mean():
-	ts = range(7)
-	ws1 = [1, 1, 1, 1, 1, 1, 1]
-	ws2 = [0, 0, 0, 2, 2, 2, 2]
-	fun = lambda x: 1. / (1 - x)
-	obj1 = procrastination.WeightedMean(fun, ts, ws1)
-	obj2 = procrastination.WeightedMean(fun, ts, ws2)
-	assert obj1.get_procrastination() < obj2.get_procrastination()
+    ts = range(7)
+    ws1 = [1, 1, 1, 1, 1, 1, 1]
+    ws2 = [0, 0, 0, 2, 2, 2, 2]
+    fun = lambda x: 1. / (1 - x)
+    obj1 = procrastination.WeightedMean(fun, ts, ws1)
+    obj2 = procrastination.WeightedMean(fun, ts, ws2)
+    assert obj1.get_procrastination() < obj2.get_procrastination()
